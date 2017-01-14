4e9c075966926adc5ffa31fb6ffeb20930afd7c4
ShabbySheek1313 <matthewdavis@MacBook-Pro.local>
2017-01-14 00:19:07 -0700

    Final Edits

---
+++ js/quotes.js
@@ -0,0 +1,167 @@
+//Creating an array of quote objects.
+
+const quotes = [
+    {
+        quote: 'You must be the change you wish to see in the world.',
+        source:'Gandhi',
+        citation:'',
+        year:'1942'
+    },
+    {
+        quote: 'You must be the change you wish to see in the world.',
+        source:'Aristotle',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it.',
+        source:'Maurice Switzer',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Don\'t cry because it\'s over, smile because it happened.',
+        source: 'Dr. Seuss',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'You only live once, but if you do it right, once is enough.',
+        source: 'Mae West',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'If you tell the truth, you don\'t have to remember anything.',
+        source: 'Mark Twain',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Always forgive your enemies; nothing annoys them so much.',
+        source: 'Oscar Wilde',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Without music, life would be a mistake.',
+        source: 'Friedrich Nietzsche',
+        citation:'Twilight of the Idols',
+        year:''
+    },
+    {
+        quote: 'It is better to be hated for what you are than to be loved for what you are not.',
+        source: 'André Gide',
+        citation:'Autumn Leaves',
+        year:''
+    },
+    {
+        quote: 'Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).',
+        source: 'Mark Twain',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'The fool doth think he is wise, but the wise man knows himself to be a fool.',
+        source: 'William Shakespeare',
+        citation:'As You Like It',
+        year:''
+    },
+    {
+        quote: 'A day without sunshine is like, you know, night.',
+        source: 'Steve Martin',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'If you judge people, you have no time to love them.',
+        source: 'Mother Teresa',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'For every minute you are angry you lose sixty seconds of happiness.',
+        source: 'Ralph Waldo EmersonRalph Waldo Emerson',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'I\'m not afraid of death; I just don\'t want to be there when it happens.',
+        source: 'Woody Allen',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Life isn\'t about finding yourself. Life is about creating yourself.',
+        source: 'George Bernard Shaw',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'The difference between genius and stupidity is: genius has its limits.',
+        source: 'Alexandre Dumas',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'I find television very educating. Every time somebody turns on the set, I go into the other room and read a book.',
+        source: 'Groucho Marx',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+        source: 'Winston S. Churchill',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'The reason I talk to myself is because I’m the only one whose answers I accept.',
+        source: 'George Carlin',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Not all those who wander are lost.',
+        source: 'J.R.R. Tolkien',
+        citation:'The Fellowship of the Ring',
+        year:''
+    },
+    {
+        quote: 'I have never let my schooling interfere with my education.',
+        source: 'Mark Twain',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Dost thou love life? Then do not squander time, for that is the stuff life is made of.',
+        source: 'Benjamin Franklin',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'We don’t see things the way they are. We see them the way WE are.',
+        source: 'Talmud',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'A journey of a thousand miles begins with a single step',
+        source: 'Lao Tzu',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Don’t ask what the world needs. Ask what makes you come alive and go do it. Because what the world needs is more people who have come alive.',
+        source: 'Howard Thurman',
+        citation:'',
+        year:''
+    },
+    {
+        quote: 'Every man dies. Not every man really lives.',
+        source: 'William Ross Wallace',
+        citation:'',
+        year:''
+    }
+
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,56 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//Setting a timer so the printQuote function will execute every 30 seconds.
+
+setInterval(printQuote,30000);
+
+//Stores the random quote object generated from the function getRandomQuote() into a variable.
+// The variable is then used to access the property values of that object to build a string containing the HTML to display the quote object.
+//Then the string is inserted into the <div> with the ID 'quote-box'.
+
+function printQuote() {
+    let ranQuote = getRandomQuote();
+    let message = '';
+    message += '<p class="quote">' + ranQuote.quote + '</p>';
+    message += '<p class="source">' + ranQuote.source;
+    if (ranQuote.citation !== ''){
+        message += '<span class="citation">' + ranQuote.citation + '</span>';
+    }
+    if (ranQuote.year !== ''){
+        message += '<span class="year">' + ranQuote.year + '</span>';
+    }
+    message += '</p>';
+    document.getElementById('quote-box').innerHTML = message;
+    document.body.style.background = ranColor();
+
+}
+
+//Creating an array of used indexes so that i can make sure no quote is repeated twice.
+//Generates a random number, checks to see if that random number index has been already used, and then returns a quote object with the index of that random number.
+//Stores that index value in the usedIndexArray.
+
+let usedIndexArray = [];
+
+function getRandomQuote() {
+    let index = Math.floor(Math.random() * (quotes.length + 1));
+    if(usedIndexArray.indexOf(index) === -1) {
+        usedIndexArray.push(index);
+    }else if(quotes.length === usedIndexArray.length){
+        usedIndexArray = [];
+    }else {
+        getRandomQuote();
+    }
+    console.log(index);
+    console.log(usedIndexArray);
+    return quotes[index];
+}
+
+//Generates a random rgb color.
+
+function ranColor() {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        return 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
